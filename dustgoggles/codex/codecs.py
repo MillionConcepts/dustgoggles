7aa8774ba652049763f4e0ba85ff7c56e1856c3b
m-stclair <mstclair@millionconcepts.com>
2022-04-23 13:11:28 -0400

    more documentation in codex.codecs

--- dustgoggles/codex/codecs.py
+++ dustgoggles/codex/codecs.py
@@ -2,38 +2,63 @@ import ast
 import json
 from multiprocessing.shared_memory import SharedMemory
 import pickle
-from typing import Any, Callable, Mapping
+from typing import Any, Callable, Mapping, Union
 
 from dustgoggles.codex.memutilz import create_block, fetch_block_bytes
 from dustgoggles.func import zero
 
 
-def json_codec_factory():
-    def encode(value):
+def json_codec_factory() -> tuple[Callable, Callable]:
+    """
+    generate a pair of encoder / decoder functions that can be inserted into
+    the memorize / remember steps of a mnemonic or used on their own.
+
+    this is a very simple codec that simply uses JSON serialization and
+    deserialization. only objects that are stable through JSON serialization
+    can/should be passed to this codec.
+    """
+    def encode(value: Any) -> bytes:
         return json.dumps(value).encode()
 
-    def decode(value):
-        return json.loads(value.decode())
+    def decode(blob: bytes) -> Any:
+        return json.loads(blob)
     return encode, decode
 
 
 def ast_codec_factory():
-    def encode(value):
+    """
+    generate a pair of encoder / decoder functions that can be inserted into
+    the memorize / remember steps of a mnemonic or used on their own.
+
+    this is a very simple codec that uses the Python abstract syntax tree
+    serializer and deserializer. It will reliably serialize a wider variety of
+    Python literals than using JSON serialization, but is much slower.
+    """
+    def encode(value: Any) -> bytes:
         return str(value).encode()
 
-    def decode(value):
-        return ast.literal_eval(value.decode())
+    def decode(blob: bytes) -> Any:
+        return ast.literal_eval(blob.decode())
     return encode, decode
 
 
 def json_pickle_codec_factory():
-    def encode(value):
+    """
+    generate a pair of encoder / decoder functions that can be inserted into
+    the memorize / remember steps of a mnemonic or used on their own.
+
+    this codec attempts to serialize passed objects as JSON. failing that,
+    it will attempt to serialize them using pickle. This is a useful generic
+    codec that tends to produce behaviors that mirror default Python
+    interprocess communication.
+    """
+    def encode(value: Any) -> bytes:
         try:
             return json.dumps(value).encode()
         except TypeError:
             return pickle.dumps(value, protocol=5)
 
-    def decode(blob):
+    def decode(blob: bytes) -> Any:
         try:
             return json.loads(blob.decode())
         except UnicodeDecodeError:
