c4fc92d7b2b65b432791045dcda005ae1b13638b
michael <mstclair@millionconcepts.com>
2022-06-01 14:57:52 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dustgoggles",
-    version="0.3.1",
+    version="0.4.0",
     url="https://github.com/millionconcepts/dustgoggles.git",
     author="Million Concepts",
     author_email="mstclair@millionconcepts.com",
